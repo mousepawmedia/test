d53b2d39f497066dd3d902b8499bfc6ba89bb84c
Jason C. McDonald <jcmcdonald@mousepawgames.com>
2016-11-05 11:37:21 -0700

    Updated configuration to new Phabricator URL.

--- FiringRange/main.cpp
+++ FiringRange/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include "rigid_stack.hpp"
 
 using std::cout;
 using std::endl;
 
 int main()
 {
-    std::cout << "Hello, world! 123 ABCDEFG" << std::endl;
+    pawlib::RigidStack<uint32_t,10> stk;
+    std::cout << "Size: " << stk.length() << std::endl;
+
+    stk.push(42);
+    stk.push(76);
+    std::cout << stk.pop() << std::endl;
+    std::cout << stk.pop() << std::endl;
+
+    for(int i=0; i < 10; i++)
+    {
+        stk.push(i+1);
+        std::cout << stk.top() << std::endl;
+    }
+
+    while(true)
+    {
+        try
+        {
+            std::cout << stk.pop() << std::endl;
+        }
+        catch(std::out_of_range)
+        {
+            break;
+        }
+    }
+
     return 0;
 }
