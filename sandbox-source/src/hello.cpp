fbdb61fe952b4c532894afe02a67518c24f4043f
Jason C. McDonald <jcmcdonald@mousepawmedia.com>
2019-12-06 17:31:50 -0800

    Updated to use new PawLIB IOC codes

--- sandbox-source/src/hello.cpp
+++ sandbox-source/src/hello.cpp
@@ -2,8 +2,8 @@
 
 void hello()
 {
-    pawlib::ioc << pawlib::IOFormatTextFG::fg_blue
-                << pawlib::IOFormatTextAttributes::ta_bold
+    pawlib::ioc << pawlib::IOFormatTextFG::blue
+                << pawlib::IOFormatTextAttr::bold
                 << "Hello, world!"
-                << pawlib::IOControl::io_end;
+                << pawlib::IOCtrl::endl;
 }
